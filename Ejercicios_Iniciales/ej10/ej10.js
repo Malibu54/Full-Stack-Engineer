cdf1fe71210de9354d16cb134531a70102296260
~Oriana~ <deciimesool@icloud.com>
2025-11-03 15:13:18 -0300

    Update ej10.js

---
+++ Ejercicios_Iniciales/ej10/ej10.js
@@ -0,0 +1,13 @@
+const auto = {
+    marca : 'Ferrari',
+    modelo : 'F8 Tributo',
+    año : 2023,
+    color : 'Rojo',
+    velocidadMaxima : 340,
+    precio : 280000,
+    combustible : "Gasolina",
+    puertas : 2
+}
+
+console.log (auto)
+
