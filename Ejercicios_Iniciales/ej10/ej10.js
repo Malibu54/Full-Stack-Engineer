d32dd0239476e97a47d1b0b4cd6d43cd3f4bfc1e
~Oriana~ <deciimesool@icloud.com>
2025-11-03 15:41:00 -0300

    Update ej10.js

--- Ejercicios_Iniciales/ej10/ej10.js
+++ Ejercicios_Iniciales/ej10/ej10.js
@@ -7,9 +7,10 @@ const auto = {
     precio : 280000,
     combustible : "Gasolina",
     puertas : 2
-}
+};
 
-console.log (auto)
+console.log (auto);
 
-let mensajeDinamico = (`El auto ${auto.marca} es uno de los autos más potentes en el mercado alcanzando una velocidad de  ${auto.velocidadMaxima} por hora!`)
+let mensajeDinamico = (`El auto ${auto.marca} es uno de los autos más potentes en el mercado alcanzando una velocidad de  ${auto.velocidadMaxima} por hora!`);
 
+document.writeln(mensajeDinamico);
