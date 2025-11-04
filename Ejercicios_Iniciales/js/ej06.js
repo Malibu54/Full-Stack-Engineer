85f13bf762bb959b506044d6494d4ccd7f77de3b
~Oriana~ <deciimesool@icloud.com>
2025-11-04 09:47:52 -0300

    Delete ej06.js

--- Ejercicios_Iniciales/js/ej06.js
+++
@@ -1,7 +0,0 @@
-let radio = prompt("Ingresar el radio del círculo:");
-
-const calculatPerimetro = 2*3.14*parseFloat(radio);
-const area = 3.14*(radio)*(radio);
-
-console.log("El perimetro es: " + (calculatPerimetro));
-console.log("El área es: " + (area) );   
