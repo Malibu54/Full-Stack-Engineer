3dd600b76a6627ba3a43b6d806f0c778fe642807
~Oriana~ <deciimesool@icloud.com>
2025-11-04 09:43:56 -0300

    Delete ej04.js

--- Ejercicios_Iniciales/ej04/ej04.js
+++
@@ -1,5 +0,0 @@
-let color = "Rosa";
-let comida = "Papas Fritas";
-let lugar = "Montevideo, Uruguay";
-
-console.log(`Mi color favorito es ${color}, me encanta comer ${comida} y quiero visitar ${lugar}.`)
