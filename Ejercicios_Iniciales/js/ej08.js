4e35a25826f48c8f18beae418c94df7876974c81
~Oriana~ <deciimesool@icloud.com>
2025-11-04 09:47:57 -0300

    Delete ej08.js

--- Ejercicios_Iniciales/js/ej08.js
+++
@@ -1,8 +0,0 @@
-
-let precioUSD = parseFloat(prompt("Ingresar precio en dolares: "));
-
-const precioPesos = precioUSD * 1530;
-const precioConIVA = precioPesos * 1.21;
-const precioFinal = precioConIVA * 1.30;
-
-alert(`El precio final es ${precioFinal}`)
