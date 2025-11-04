bf4fd7ce8dab857bd2edca6c10875731c6a69a66
~Oriana~ <deciimesool@icloud.com>
2025-11-04 09:47:50 -0300

    Delete ej05.js

--- Ejercicios_Iniciales/js/ej05.js
+++
@@ -1,8 +0,0 @@
-let numero1 = parseFloat(prompt("Ingresar un numero: "));
-let numero2 = parseFloat(prompt("Ingresar otro numero: "));
-let numero3 = parseFloat(prompt("E Ingresar otro numero: "));
-
-const promedio = (numero1 + numero2 + numero3) / 3;
-
-console.log(promedio);
-
