d2e2a6f748be99a535c54888cff9f966811a006c
~Oriana~ <deciimesool@icloud.com>
2025-12-13 18:59:05 -0300

    Update Ejercicio11.js

--- Ejercicios_Iniciales/js/Ejercicio11.js
+++ Ejercicios_Iniciales/js/Ejercicio11.js
@@ -1,8 +1,41 @@
-let listaDeCompras = ["Pan",
-    "leche",
-    "huevos",
-    "arroz",
-    "manzanas",
-    "pollo",
-    "queso"];
+class Producto {
+
+    constructor(nombre, precio, stock) {
+
+        this.nombre = nombre
+
+        this.precio = parseFloat(precio)
+
+        this.stock = parseInt(stock)
+
+    }
+
+    calcularPrecioFinal(cantidadPedida) {
+
+
+        let cantidadPedida = number
+
+        if (cantidadPedida > this.stock) 
+        {
+
+            return `Error: Stock insuficiente. Solo quedan ${this.stock} unidades.`
+
+        } else 
+        {
+
+            let precioBase = this.precio * cantidadPedida
+
+        }else if (cantidadPedida >= 0)
+        {
+
+            precioFinal = precioBase * 10 / 100
+
+            console.log(precioFinal)
+
+        }
+
+    }
+}
+
+
 
