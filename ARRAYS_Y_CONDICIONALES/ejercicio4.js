dc9a16dbc850d686cfd139be657782bffa270fcb
~Oriana~ <deciimesool@icloud.com>
2025-12-26 15:09:23 -0300

    Update ejercicio4.js

--- ARRAYS_Y_CONDICIONALES/ejercicio4.js
+++ ARRAYS_Y_CONDICIONALES/ejercicio4.js
@@ -1,3 +1,17 @@
 // no tiene que ser perfecto
 // esta bien si no se o me equivoco
 // solo tiene que existir
+
+/***
+ * 
+ * Dado un array de números [23, 45, 12, 67, 34, 89, 21], escribe un programa que use un bucle para encontrar el número más grande. Usa condicionales para comparar los valores
+ */
+
+let numeros = [23, 45, 12, 67, 34, 89, 21]
+
+for (let i = 1; i <= 90; i++) {
+    let resultado = i * numero;
+    if (resultado > 1000) {
+        break;
+    }
+    console.log(numero + " x " + i + " = " + resultado);
