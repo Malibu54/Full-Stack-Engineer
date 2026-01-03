e64398c95208968b2b9d7892e3653fe25c8157ab
~Oriana~ <deciimesool@icloud.com>
2026-01-03 20:50:54 -0300

    Update contactManager.js

--- LISTA_DE_CONTACTOS/js/contactManager.js
+++ LISTA_DE_CONTACTOS/js/contactManager.js
@@ -1,1 +1,10 @@
-// # Gestión del CRUD de contactos
+/**
+# Gestión del CRUD de contactos
+ * addContact() - Agregar nuevo contacto
+ * updateContact() - Actualizar contacto existente
+ * deleteContact() - Eliminar contacto
+ * getContacts() - Obtener todos los contactos
+ * getContactById() - Obtener un contacto específico
+ * saveToLocalStorage() - Persistir datos
+ * loadFromLocalStorage() - Cargar datos
+ * */ 
