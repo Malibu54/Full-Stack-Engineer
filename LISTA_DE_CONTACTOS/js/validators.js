4a3560812dcf5f5d7f221ab34b5879c89d9d9c7f
~Oriana~ <deciimesool@icloud.com>
2026-01-03 20:50:57 -0300

    Update validators.js

--- LISTA_DE_CONTACTOS/js/validators.js
+++ LISTA_DE_CONTACTOS/js/validators.js
@@ -1,1 +1,8 @@
-// # Validaciones de formularios
+/**
+# Validaciones de formularios
+ * validateName() - Validar nombre
+ * validateEmail() - Validar formato de email
+ * validatePhone() - Validar teléfono
+ * validateDate() - Validar fecha de nacimiento
+ * validateForm() - Validación completa del formulario
+ * */ 
