b7654a57d19d987754f716ae526ba5fd74e56ee2
~Oriana~ <deciimesool@icloud.com>
2026-01-03 19:28:55 -0300

    Update ejercicio8.js

--- ARRAYS_Y_CONDICIONALES/ejercicio8.js
+++ ARRAYS_Y_CONDICIONALES/ejercicio8.js
@@ -1,3 +1,14 @@
-// no tiene que ser perfecto
-// esta bien si no se o me equivoco
-// solo tiene que existir
+let num = parseInt(prompt("Ingresa un número entero positivo: "));
+let invertido = 0;
+
+if (num < 0) {
+  console.log("El número debe ser positivo");
+} else {
+  while (num != 0) {
+    let digito = num % 10;
+    invertido = invertido * 10 + digito;
+    num = Math.floor(num / 10);
+  }
+
+  console.log("El número invertido es: " + invertido);
+}
