f7c3f05149f43d1c46cc568ab7fa97b7edf6165c
~Oriana~ <deciimesool@icloud.com>
2026-01-03 19:46:17 -0300

    Update ejercicio10.js

--- ARRAYS_Y_CONDICIONALES/ejercicio10.js
+++ ARRAYS_Y_CONDICIONALES/ejercicio10.js
@@ -1,4 +1,16 @@
-// no tiene que ser perfecto
-// esta bien si no se o me equivoco
-// solo tiene que existir
+let entrada = prompt("Ingresar palabras separadas por comas: ");
 
+let palabras = entrada.split(",");
+
+let palindromas = [];
+
+for (let i = 0; i < palabras.length; i++){
+    let palabra = palabras [i].trim();
+    let alreves = palabra.split("").reverse().join("");
+    if(palabra.toLocaleLowerCase()===alreves.toLocaleLowerCase()){
+        palindromas.push(palabra);
+    }
+}
+
+console.log("Palabras palindromas encontradas: ")
+console.log(palindromas);
