42730988ea18f87b2a1b31e1413e2bb2279f2bb7
~Oriana~ <deciimesool@icloud.com>
2026-01-03 17:44:06 -0300

    Update ejercicio5.js

---
+++ ARRAYS_Y_CONDICIONALES/ejercicio5.js
@@ -0,0 +1,29 @@
+
+let filas = ["*"];
+
+switch (filas) {
+  case '*':
+    console.log("*");
+    break;
+  case '**':
+    console.log("**");
+    break;
+  case '***': 
+    console.log("***");
+    break;
+  case '****':
+    console.log("****");
+    break;
+  case '*****':
+    console.log("*****");
+    break;
+  case '******':
+    console.log("******");
+    break;
+  case '*******':
+    console.log("*******");
+    break;
+  default: 
+    console.log("Fila no válida o no registrada.");
+}
+
