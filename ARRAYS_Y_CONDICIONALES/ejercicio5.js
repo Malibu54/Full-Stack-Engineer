a0b4b1b94b61908a79ffab7a8ab9c1a18fd16af0
~Oriana~ <deciimesool@icloud.com>
2026-01-03 17:58:26 -0300

    Update ejercicio5.js

--- ARRAYS_Y_CONDICIONALES/ejercicio5.js
+++ ARRAYS_Y_CONDICIONALES/ejercicio5.js
@@ -1,28 +1,11 @@
-let filas = "*";
 
-switch (filas) {
-  case '1':
-    console.log("*");
-    break;
-  case '2':
-    console.log("**");
-    break;
-  case '3': 
-    console.log("***");
-    break;
-  case '4':
-    console.log("****");
-    break;
-  case '5':
-    console.log("*****");
-    break;
-  case '6':
-    console.log("******");
-    break;
-  case '7':
-    console.log("*******");
-    break;
-  default: 
-    console.log("Fila no válida o no registrada.");
+
+let filas = parseInt(prompt("¿Cuántos asteriscos queres? (1-7):"));
+
+for (let i = filas; i >= 1; i--) {
+  let linea = "";
+  for (let j = 0; j < i; j++) {
+    linea += "*";
+  }
+  console.log(linea);
 }
-
