6f98a66e1b043c85adad768db9c3c786e7f94716
~Oriana~ <deciimesool@icloud.com>
2026-01-03 17:47:02 -0300

    Update ejercicio5.js

--- ARRAYS_Y_CONDICIONALES/ejercicio5.js
+++ ARRAYS_Y_CONDICIONALES/ejercicio5.js
@@ -1,5 +1,22 @@
+// no tiene que ser perfecto
+// esta bien si no se o me equivoco
+// solo tiene que existir
 
-let filas = ["*"];
+/**Crea un programa que use un bucle para generar un triángulo de asteriscos con la siguiente forma, donde el número de filas lo elija el usuario:
+ * 
+ * Inicio
+
+  Leer n   // número de filas elegido por el usuario
+
+  Para i desde n hasta 1 hacer
+      Imprimir "*" repetido i veces
+  Fin Para
+
+Fin
+ */
+
+
+let filas = "*";
 
 switch (filas) {
   case '*':
