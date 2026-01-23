cf85ac59a2466567a04d39ad0fe088dcdffbeb91
~Oriana~ <deciimesool@icloud.com>
2026-01-23 17:48:37 -0300

    Update App.jsx

--- Modulo-03-React/01-components/src/App.jsx
+++ Modulo-03-React/01-components/src/App.jsx
@@ -1,11 +1,13 @@
+import Title from "./components/Title/Title";
+
 export default function App (){
 
  //Contenido del componente App
  //Toda la l[ogica y el JSX  para renderizar
  
  return (
-  <> //Etiqueta React.Fragment
-  <h1>Hola Vite</h1>
+  <> 
+  <Title/>
   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta veritatis molestias repudiandae sapiente commodi vero aliquid iusto provident aut eveniet.</p>
   </>
  )
