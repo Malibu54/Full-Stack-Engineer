c8d114f2ad951a41b307ec49f8450f706b8e3617
~Oriana~ <deciimesool@icloud.com>
2026-01-23 17:40:39 -0300

    Update App.jsx

---
+++ Modulo-03-React/01-components/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App (){
+
+ // contenido del componente App
+ //Toda la l[ogica y el JSX  para renderizar
+ 
+ return (
+  <div>
+  <h1>Hola Vite</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta veritatis molestias repudiandae sapiente commodi vero aliquid iusto provident aut eveniet.</p>
+  </div>
+ )
+
+}
