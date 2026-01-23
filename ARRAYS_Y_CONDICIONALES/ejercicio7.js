834c0f25b0793f7738c9a49be016def9b47ef156
~Oriana~ <deciimesool@icloud.com>
2026-01-23 11:29:08 -0300

    Delete ejercicio7.js

--- ARRAYS_Y_CONDICIONALES/ejercicio7.js
+++
@@ -1,8 +0,0 @@
-let acumulado = 0;
-
-while (acumulado <= 100) {
-  let numero = parseInt(prompt("Ingresa un número entero: "));
-  acumulado += numero;
-}
-
-console.log("Total acumulado: " + acumulado);
