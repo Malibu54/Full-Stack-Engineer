4520552005590238cdb7baa6742c5814570e5e87
~Oriana~ <deciimesool@icloud.com>
2026-01-23 12:06:30 -0300

    Delete Ejercicio2.js

--- Ejercicios_Iniciales/js/Ejercicio2.js
+++
@@ -1,6 +0,0 @@
-let base = prompt("Ingrese base:");
-let altura = prompt("Ingrese altura:");
-
-const area = parseFloat(base) * parseFloat(altura) 
-
-console.log("El área del rectángulo es " + area)
