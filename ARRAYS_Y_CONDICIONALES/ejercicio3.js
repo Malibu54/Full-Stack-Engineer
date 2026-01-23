6ae92305ec47d9a7ed184690873d648fbc906125
~Oriana~ <deciimesool@icloud.com>
2026-01-23 11:28:59 -0300

    Delete ejercicio3.js

--- ARRAYS_Y_CONDICIONALES/ejercicio3.js
+++
@@ -1,9 +0,0 @@
-let numero = Number(prompt("Introduce un número del 1 al 100:"));
-
-for (let i = 1; i <= 100; i++) {
-    let resultado = i * numero;
-    if (resultado > 1000) {
-        break;
-    }
-    console.log(numero + " x " + i + " = " + resultado);
-}
