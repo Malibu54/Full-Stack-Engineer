a71acece9a5ae6efcb863e5910717657d6c494dd
~Oriana~ <deciimesool@icloud.com>
2026-01-23 17:48:43 -0300

    Create Title.jsx

---
+++ Modulo-03-React/01-components/src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+function Title(){
+
+    return(
+        <>
+        <h1>Component Title</h1>
+        <hr />
+        <h2>Subtitulo del Componente</h2>
+        </>
+    )
+}
+
+export default Title
